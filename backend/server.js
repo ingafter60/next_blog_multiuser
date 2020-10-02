1f635b25deb4437a580ab678d0517a73fb09fea0
ing <ingafter60@gmail.com>
2020-10-02 08:57:48 +0700

    3. Setup the server

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+// import modules
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+// use express module to create app
+const app = express()
+
+// add middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+// use cors to communicate backend and frontend
+app.use(cors())
+
+// routes
+app.get('/api', (req, res) => {
+  res.json({time: Date().toString()})
+})
+
+// port
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`The server is running on http://localhost:8000 using PORT ${port}`)
+})
