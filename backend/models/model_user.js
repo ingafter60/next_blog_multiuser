5299902d87dd6390b34a61c7bde52c8561c057b6
ing <ingafter60@gmail.com>
2020-10-02 10:53:32 +0700

    1. User model and schema

---
+++ backend/models/model_user.js
@@ -0,0 +1,54 @@
+// import modules
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+
+const userShcema = new mongoose.Schema({
+  usename: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 32,
+    unique: true,
+    index: true,
+    lowercase: true
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    max: 32
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  profile: {
+    type: String,
+    required: true
+  },
+  hashed_password: {
+    type: String,
+    required: true
+  },
+  salt: String,
+  about: {
+    type: String
+  },
+  role: {
+    type: Number,
+    trim: true
+  },
+  photo: {
+    data: Buffer,
+    contentType: String
+  },
+  resetPasswordLink: {
+    data: String,
+    default: ''
+  }
+}, {timestamps: true})
+
+module.exports = mongoose.model('User', userShcema)
