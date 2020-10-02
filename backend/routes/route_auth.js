e3d0ee58906339c24a9f07d41649b6534c2f0b32
ing <ingafter60@gmail.com>
2020-10-02 14:48:33 +0700

    4. Singup validator

--- backend/routes/route_auth.js
+++ backend/routes/route_auth.js
@@ -5,6 +5,11 @@ const router = express.Router()
 // import controller
 const {signup} = require('../controllers/controller_auth')
 
-router.post('/', signup)
+// validators
+const { runValidation } = require('../validators');
+const { userSignupValidator } = require('../validators/auth');
+
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router
+
