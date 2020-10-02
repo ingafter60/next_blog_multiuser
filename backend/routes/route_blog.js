bd365f649638f3ed049464e890bd4fa0265eb004
ing <ingafter60@gmail.com>
2020-10-02 10:18:59 +0700

    7. Controllers

--- backend/routes/route_blog.js
+++ backend/routes/route_blog.js
@@ -2,8 +2,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({time: Date().toString()})
-})
+// import controller
+const {time} = require('../controllers/controller_blog')
+
+router.get('/', time)
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ if(process.env.NODE_ENV == 'development'){
 }
 
 // routes middleware
-app.use(blogRoutes)
+app.use('/api', blogRoutes)
 
 // port
 const port = process.env.PORT || 8000
