c34665307440b034a1b679c93fadb380e513839b
ing <ingafter60@gmail.com>
2020-10-02 15:33:12 +0700

    6. User signup

--- backend/controllers/controller_auth.js
+++ backend/controllers/controller_auth.js
@@ -1,7 +1,39 @@
+const User = require('../models/model_user')
+const shortId = require('shortid')
+
 exports.signup = (req, res) => {
-  const { name, email, password } = req.body
-  res.json({
-    user:{ name, email, password }
+  // console.log(req.body)
+  // find the signup user based on its email
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+      
+    // if user already exist 
+    if (user) {
+      return res.status(400).json({
+          error: 'Email is taken'
+      })
+    }
+
+    // if the signing user is not exist
+    const { name, email, password } = req.body
+    let username = shortId.generate()
+    let profile = `${process.env.CLIENT_URL}/profile/${username}`
+
+    let newUser = new User({ name, email, password, profile, username })
+    newUser.save((err, success) => {
+      if (err) {
+        return res.status(400).json({
+            error: err
+        })
+      }
+      // RETURN USER DATA AFTER SIGNUP
+      // res.json({
+      //     user: success
+      // })
+
+      // RETURN SUCCESS MESSAGE AFER SIGNUP
+      res.json({
+        message: 'Signup success! Please signin.'
+      })
+    })
   })
 }
-
--- backend/models/model_user.js
+++ backend/models/model_user.js
@@ -1,55 +1,58 @@
 // import modules
-const mongoose = require('mongoose')
-const crypto = require('crypto')
+const mongoose = require('mongoose');
+const crypto = require('crypto');
 
-const userShcema = new mongoose.Schema({
-  usename: {
-    type: String,
-    trim: true,
-    required: true,
-    max: 32,
-    unique: true,
-    index: true,
-    lowercase: true
-  },
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    max: 32
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-    lowercase: true
-  },
-  profile: {
-    type: String,
-    required: true
-  },
-  hashed_password: {
-    type: String,
-    required: true
-  },
-  salt: String,
-  about: {
-    type: String
-  },
-  role: {
-    type: Number,
-    trim: true
-  },
-  photo: {
-    data: Buffer,
-    contentType: String
-  },
-  resetPasswordLink: {
-    data: String,
-    default: ''
-  }
-}, {timestamps: true})
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 32,
+            unique: true,
+            index: true,
+            lowercase: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 32
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        profile: {
+            type: String,
+            required: true
+        },
+        hashed_password: {
+            type: String,
+            required: true
+        },
+        salt: String,
+        about: {
+            type: String
+        },
+        role: {
+            type: Number,
+            trim: true
+        },
+        photo: {
+            data: Buffer,
+            contentType: String
+        },
+        resetPasswordLink: {
+            data: String,
+            default: ''
+        }
+    },
+    { timestamp: true }
+)
 
 userSchema
     .virtual('password')
@@ -87,4 +90,4 @@ userSchema.methods = {
     }
 }
 
-module.exports = mongoose.model('User', userShcema)
+module.exports = mongoose.model('User', userSchema)
